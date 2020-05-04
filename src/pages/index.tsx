40b6308e57227fbfbd2e2ed2303d041c85ce721d
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-05-04 13:25:12 +0530

    add some state to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import { useState } from 'react';
+
 export default () => {
-  return <div className="text-4xl text-center py-4">Hello!</div>;
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="py-8 flex justify-center">
+      <button
+        className="text-2xl text-center border-2 px-6 py-3 rounded-lg hover:underline"
+        onClick={() => setCounter(counter + 1)}
+      >
+        You've clicked this button {counter} times!
+      </button>
+    </div>
+  );
 };
