201f569bd46a2832d3226d58344d19b19a85f541
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-05-04 18:38:26 +0530

    add <title> to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
+import Head from 'next/head';
 
 export default () => {
   const [counter, setCounter] = useState(0);
   return (
-    <div className="flex-1 flex justify-center items-center">
-      <button
-        className="px-6 sm:px-10 py-5 bg-white text-purple-600 sm:text-lg rounded-full shadow-lg focus:outline-none transition duration-150 transform hover:-translate-y-1 hover:bg-gray-300 hover:scale-105"
-        onClick={() => setCounter(counter + 1)}
-      >
-        You've clicked this button {counter} times!
-      </button>
-    </div>
+    <>
+      <Head>
+        <title>Next TypeScript Tailwind Starter</title>
+      </Head>
+      <div className="flex-1 flex justify-center items-center">
+        <button
+          className="px-6 sm:px-10 py-5 bg-white text-purple-600 sm:text-lg rounded-full shadow-lg focus:outline-none transition duration-150 transform hover:-translate-y-1 hover:bg-gray-300 hover:scale-105"
+          onClick={() => setCounter(counter + 1)}
+        >
+          You've clicked this button {counter} times!
+        </button>
+      </div>
+    </>
   );
 };
