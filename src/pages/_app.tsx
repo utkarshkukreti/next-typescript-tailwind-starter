deab8105c387110b248aacff7f059677526bd23a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-05-04 13:13:52 +0530

    use `AppProps` from `next/app` in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
+import { AppProps } from 'next/app';
+
 import '../index.css';
 import '../../node_modules/typeface-work-sans';
 
-export default <Props extends any>({
-  Component,
-  pageProps,
-}: {
-  Component: React.FC<Props>;
-  pageProps: Props;
-}) => (
+export default ({ Component, pageProps }: AppProps) => (
   <div className="bg-purple-600 text-purple-50 min-h-screen">
     <Component {...pageProps} />
   </div>
