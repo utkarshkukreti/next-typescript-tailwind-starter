bf45e473b9ab743418dbc426aae779bd9999091f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-05-12 09:15:31 +0530

    use next 9.4's new baseUrl feature [1]
    [1]: https://nextjs.org/blog/next-9-4#absolute-imports-and-aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 
-import '../index.css';
-import '../../node_modules/typeface-work-sans';
+import 'index.css';
+import 'typeface-work-sans';
 
 export default ({ Component, pageProps }: AppProps) => (
   <div className="bg-purple-500 text-purple-50 min-h-screen flex flex-col">
