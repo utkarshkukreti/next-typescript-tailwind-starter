ea2d65f6b1e635e08f20276d54a92f16958d8a33
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-07-12 19:02:35 +0530

    this is the first thing I remove when I use this starter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import '@/index.css';
 import 'typeface-work-sans';
 
 export default ({ Component, pageProps }: AppProps) => (
-  <div className="bg-cool-gray-200 min-h-screen antialiased">
+  <div className="antialiased">
     <Component {...pageProps} />
   </div>
 );
