d6605df0da200f20e4198874eb6e41ab0deafe63
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-07-22 23:07:17 +0530

    this is better than allowing any P and using `P & {}`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import '@/index.css';
 import 'typeface-work-sans';
 import { WithLayoutOptional } from '@/withLayout';
 
-type Props<P> = {
+type Props<P extends {}> = {
   Component: WithLayoutOptional<P>;
-  pageProps: P & {};
+  pageProps: P;
 };
 
-export default <P extends any>({ Component, pageProps }: Props<P>) =>
+export default <P extends {}>({ Component, pageProps }: Props<P>) =>
   Component.Layout ? (
     <Component.Layout>
       <Component {...pageProps} />
