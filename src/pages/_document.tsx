188bad61ba170be40dd9d6dade6b45e7d6048d19
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-07-27 23:37:58 +0530

    add custom document component

---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+
+type Props = {};
+
+class Document extends NextDocument<Props> {
+  static async getInitialProps(
+    context: DocumentContext,
+  ): Promise<DocumentInitialProps & Props> {
+    const initialProps = await NextDocument.getInitialProps(context);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
