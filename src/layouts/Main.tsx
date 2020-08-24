04c5f803f44e38c2a0805c91ee139c29418f5807
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-08-24 22:34:56 +0530

    add meta description to MainLayout; remove duplicated <Head>

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,14 +2,17 @@ import Head from 'next/head';
 
 const Main = ({
   title,
+  description,
   children,
 }: {
   title: string;
+  description: string;
   children: React.ReactNode;
 }) => (
   <>
     <Head>
       <title>{title}</title>
+      <meta name="description" content={description} />
     </Head>
     <div>{children}</div>
   </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,23 +6,19 @@ import WithLayout from '@/WithLayout';
 const Index = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <>
-      <Head>
-        <title>Untitled</title>
-        <meta name="description" content="Untitled Page" />
-      </Head>
-      <div className="flex justify-center items-center min-h-screen">
-        <button
-          className="px-6 sm:px-10 py-5 bg-purple-600 hover:bg-purple-500 text-purple-50 sm:text-lg rounded-full shadow focus:outline-none transition duration-150 transform hover:-translate-y-1 hover:scale-105"
-          onClick={() => setCounter(counter + 1)}
-        >
-          You've clicked this button {counter} times!
-        </button>
-      </div>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <button
+        className="px-6 sm:px-10 py-5 bg-purple-600 hover:bg-purple-500 text-purple-50 sm:text-lg rounded-full shadow focus:outline-none transition duration-150 transform hover:-translate-y-1 hover:scale-105"
+        onClick={() => setCounter(counter + 1)}
+      >
+        You've clicked this button {counter} times!
+      </button>
+    </div>
   );
 };
 
 export default WithLayout(Index, page => (
-  <MainLayout title="Home">{page}</MainLayout>
+  <MainLayout title="Home" description="Home Page">
+    {page}
+  </MainLayout>
 ));
