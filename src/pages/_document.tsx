ac3cdb8081559934f670968c7b8fd36dd90dcc91
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-11-10 19:58:54 +0530

    simplify /src/pages/_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,6 @@
-import NextDocument, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-type Props = {};
-
-class Document extends NextDocument<Props> {
-  static async getInitialProps(
-    context: DocumentContext,
-  ): Promise<DocumentInitialProps & Props> {
-    const initialProps = await NextDocument.getInitialProps(context);
-    return { ...initialProps };
-  }
-
+export default class extends Document {
   render() {
     return (
       <Html lang="en">
@@ -31,5 +15,3 @@ class Document extends NextDocument<Props> {
     );
   }
 }
-
-export default Document;
