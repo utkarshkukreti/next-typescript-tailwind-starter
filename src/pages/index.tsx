6a928831e799df91eaa05e67e287d4b3ab89844a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-11-20 16:57:41 +0530

    duration is 150 by default in Tailwind 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Index = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <button
-        className="px-6 sm:px-10 py-5 bg-fuchsia-600 hover:bg-fuchsia-500 text-fuchsia-50 sm:text-lg rounded-full shadow focus:outline-none focus:ring-4 ring-fuchsia-300 transition duration-150 transform hover:-translate-y-1 hover:scale-105"
+        className="px-6 sm:px-10 py-5 bg-fuchsia-600 hover:bg-fuchsia-500 text-fuchsia-50 sm:text-lg rounded-full shadow focus:outline-none focus:ring-4 ring-fuchsia-300 transition transform hover:-translate-y-1 hover:scale-105"
         onClick={() => setCounter(counter + 1)}
       >
         You've clicked this button {counter} times!
