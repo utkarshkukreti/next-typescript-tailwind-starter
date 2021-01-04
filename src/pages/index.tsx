dab62a99e3f7bf785227fb59159b8ae55c8e2d0a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-01-04 22:13:02 +0530

    make the button blue!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <button
-        className="px-8 py-5 sm:px-12 sm:py-7 bg-fuchsia-600 hover:bg-fuchsia-500 text-fuchsia-50 sm:text-xl rounded-full shadow focus:outline-none focus:ring-4 ring-fuchsia-300 transition transform hover:-translate-y-1 hover:scale-105"
+        className="px-8 py-5 sm:px-12 sm:py-7 bg-blue-600 hover:bg-blue-500 text-blue-50 sm:text-xl rounded-full shadow focus:outline-none focus:ring-4 ring-blue-300 transition transform hover:-translate-y-1 hover:scale-105"
         onClick={() => setCounter(counter + 1)}
       >
         You've clicked this button {counter} times!
