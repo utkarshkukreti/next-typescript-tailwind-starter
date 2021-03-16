269b73096e644aaa1580fbd5b8f2b0fbaeda2eaa
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-03-16 16:21:36 +0530

    new sample app on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 
 const Page = () => {
-  const [counter, setCounter] = useState(0)
+  const [name, setName] = useState('Human')
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <button
-        className="px-8 py-5 sm:px-12 sm:py-7 bg-blue-600 hover:bg-blue-500 text-blue-50 sm:text-xl rounded-full shadow focus:outline-none focus:ring-4 ring-blue-300 transition transform hover:-translate-y-1 hover:scale-105"
-        onClick={() => setCounter(counter + 1)}
-      >
-        You've clicked this button {counter} times!
-      </button>
+    <div className="max-w-4xl mx-auto p-6">
+      <div className="flex items-center">
+        <label htmlFor="name">Your Name:</label>
+        <div className="mr-4" />
+        <input
+          className="w-64"
+          type="text"
+          id="name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+      </div>
+      <div className="mb-6" />
+      <h1 className="text-3xl">
+        Hello, <span className="text-blue-600">{name}</span>!
+      </h1>
     </div>
   )
 }
