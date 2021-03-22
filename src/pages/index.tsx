a1ed393678736ec671ac242738b812b13c08ec35
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-03-22 14:22:47 +0530

    make / a server rendered page with simpler default content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,51 @@
+import { GetServerSideProps } from 'next'
 import { useState } from 'react'
 
-const Page = () => {
-  const [message, setMessage] = useState('')
+type Props = {
+  name: string
+}
 
-  const [messages, setMessages] = useState([
-    'How do you write a web application?',
-    'First you write the code and then you go to "Save As..." to save it on the web.',
-    'Which language should I use?',
-    'That depends on your business.',
-    "I'm in finance.",
-    'Then you should use FORTRAN.',
-  ])
+const Page = ({ name: initialName }: Props) => {
+  const [name, setName] = useState(initialName)
 
   return (
-    <div className="max-w-2xl mx-auto p-6 h-screen flex flex-col">
-      <div className="mt-auto flex flex-col-reverse gap-y-3 overflow-scroll">
-        {messages
-          .slice()
-          .reverse()
-          .map((message, index) => (
-            <div
-              key={index}
-              className={`px-4 py-2 rounded-xl max-w-3/4 ${
-                (messages.length - index) % 2 === 0
-                  ? 'self-start bg-gray-100'
-                  : 'self-end bg-green-100'
-              }`}
-              style={{ maxWidth: '75%' }}
-            >
-              {message}
-            </div>
-          ))}
+    <div className="max-w-4xl mx-auto p-6">
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl">
+          Hello, <span className="text-red-600">{name}</span>!
+        </h1>
+        <div className="mb-6" />
+        <form action="/" className="flex items-center">
+          <label htmlFor="name">Your Name:</label>
+          <div className="mr-4" />
+          <input
+            className="w-48 md:w-64"
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+          <div className="mr-4" />
+          <input
+            className="px-4 py-2 bg-gray-200 rounded-none focus:outline-none focus:ring-blue-600 focus:ring-2 active:bg-gray-300"
+            type="submit"
+          />
+        </form>
       </div>
-      <div className="mb-6" />
-      <form
-        className="flex items-center"
-        onSubmit={event => {
-          event.preventDefault()
-          if (!message.trim()) return
-          setMessages([...messages, message])
-          setMessage('')
-        }}
-      >
-        <input
-          className="flex-1"
-          type="text"
-          placeholder="Type something here..."
-          value={message}
-          onChange={event => setMessage(event.target.value)}
-        />
-        <div className="mr-4" />
-        <input
-          className="px-4 py-2 bg-gray-200 rounded-none focus:outline-none focus:ring-blue-600 focus:ring-2 active:bg-gray-300"
-          type="submit"
-          value="Send"
-        />
-      </form>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async context => {
+  const name =
+    typeof context.query.name === 'string' ? context.query.name : 'Nexter'
+
+  return {
+    props: {
+      name,
+    },
+  }
 }
 
 export default Page
