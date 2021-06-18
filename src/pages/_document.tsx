464e76482d338cbc3fc6bb3566ae2d7959961b97
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-06-18 06:32:03 +0530

    fix eslint warning

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-export default class extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
